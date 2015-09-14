eab9ad2f2806029d7005694def82b8026bbe6415
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-14 17:58:17 +0100

    position is autolocated and coords are pushed into an array. However, not sure of how to handle the scope for this array so it's accessible everywhere else. This might become clearer once Angular is properly integrated into the page. This collection of functions might best be used as a 'service' or 'factory'.

--- geolocation.js
+++ geolocation.js
@@ -1,33 +1,19 @@
-function verify_geolocation_capability() {
-   if (navigator.geolocation) {
-      document.form1.capability.value = "Enabled";
-   }
-   else {
-      document.form1.capability.value = "Not Enabled";
-   }
-}
-
-function display_geolocation_properties(position) {
-   var latAndLong = [];
-   document.form1.capability.value = "W o r k i n g . . .";
-   document.form1.latitude.value = position.coords.latitude;
-   document.form1.longitude.value = position.coords.longitude;
-   latAndLong.push(position.coords.latitude);
-   latAndLong.push(position.coords.longitude);
-   console.log(latAndLong);
+function buildPositionArray(position) {
+   var latLong = [];
+   latLong.push(position.coords.latitude);
+   latLong.push(position.coords.longitude);
+   console.log(latLong);
 }
 
 function handle_error(error) {
-   document.form1.capability.value = "ERROR: " + error.code;
+   console.alert("ERROR: " + error.code);
 }
 
 function retrieve_location() {
    if (navigator.geolocation) {
-      document.form1.capability.value = "Starting...";
-      navigator.geolocation.getCurrentPosition(display_geolocation_properties, handle_error);
-      document.form1.capability.value = "Finished";
+      navigator.geolocation.getCurrentPosition(buildPositionArray, handle_error);
    }
    else {
-      alert("This browser does not support geolocation services.");
+      console.alert("This browser does not support geolocation services.");
    }
 }
