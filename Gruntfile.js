43a7a9b5c21f2f96d999e2b38fbcdca7ac221822
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-15 23:55:25 +0100

    install grunt jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,16 @@ module.exports = function(grunt) {
     watch: {
       files: ['sass/app.scss', 'sass/_variables.scss'],
       tasks: ['sass'],
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -1,15 +1,7 @@
 beerStalker.factory('ApiCall', function() {
   return {
     search: function() {
-
-    }
-  };
-});
-
-beerStalker.controller('BeerStalkController', ['$scope', '$resource', function($scope, $resource) {
-
-  $scope.search = function() {
-    var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key&:text_format', {
+      var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key&:text_format', {
         text_format: "text_format=plain",
         text: "text=free+beer",
         country: "country=gb",
@@ -18,32 +10,40 @@ beerStalker.controller('BeerStalkController', ['$scope', '$resource', function($
         callback: 'JSON_CALLBACK' },
         { get: { method: 'JSONP'} });
 
-    searchResource.get().$promise.then(function(response){
-      var filteredResults = [];
-      for (index = 0; index < response.results.length; index++) {
-        var result = response.results[index].description;
-        
-        try {
-          result.indexOf('free beer')
-        } 
-        catch(err) {
-          $('#error_div').fadeIn(500).delay(8000).fadeOut(1000);
-          break;
+      searchResource.get().$promise.then(function(response){
+        var filteredResults = [];
+        for (index = 0; index < response.results.length; index++) {
+          var result = response.results[index].description;
+          
+          try {
+            result.indexOf('free beer')
+          } 
+          catch(err) {
+            $('#error_div').fadeIn(500).delay(8000).fadeOut(1000);
+            break;
+          }
+
+          if(result.indexOf('free beer') >= 0) {
+            filteredResults.push(response.results[index]);
+          }
         }
 
-        if(result.indexOf('free beer') >= 0) {
-          filteredResults.push(response.results[index]);
+        if(filteredResults.length === 0) {
+          $('.results').hide();
+          $('#no_results_div').fadeIn(500).delay(8000).fadeOut(1000);
+          return
+        } else {
+          $('.results').show();
+          $scope.searchResult = filteredResults
         }
-      }
+      });
+    } 
+  };
+});
 
-      if(filteredResults.length === 0) {
-        $('.results').hide();
-        $('#no_results_div').fadeIn(500).delay(8000).fadeOut(1000);
-        return
-      } else {
-        $('.results').show();
-        $scope.searchResult = filteredResults
-      }
-    });
+beerStalker.controller('BeerStalkController', ['$scope', '$resource', function($scope, $resource) {
+
+  $scope.search = function() {
+
   };
 }]);
